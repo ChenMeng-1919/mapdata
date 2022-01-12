10a74f74f6f3bb100fca822d690d6d2503f9fa10
cm <chenmeng_1919@163.com>
2022-01-12 19:40:13 +0800

    修改mybatis依赖，调整导入优化

--- src/main/java/com/a5ibug/lifemap/service/impl/MapServiceImpl.java
+++ src/main/java/com/a5ibug/lifemap/service/impl/MapServiceImpl.java
@@ -1,13 +1,11 @@
 package com.a5ibug.lifemap.service.impl;
 
-import com.a5ibug.lifemap.mapper.MapMapper;
 import com.a5ibug.lifemap.entity.Map;
+import com.a5ibug.lifemap.mapper.MapMapper;
 import com.a5ibug.lifemap.service.MapService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Service
--- src/main/java/com/a5ibug/lifemap/service/impl/UserServiceImpl.java
+++ src/main/java/com/a5ibug/lifemap/service/impl/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.a5ibug.lifemap.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Service
