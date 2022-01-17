50ec8786e19341b370000c1c160314748d9fa289
Meng Chen <chenmeng_1919@163.com>
2022-01-17 18:48:30 +0800

    修改页面跳转代码

--- src/main/java/com/a5ibug/lifemap/controller/QueryDataController.java
+++ src/main/java/com/a5ibug/lifemap/controller/QueryDataController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +17,7 @@ import java.util.Map;
  * @date: Created in 2022/1/17 18:27
  * @description:
  */
-@RestController
+@Controller
 @CrossOrigin
 @Slf4j
 public class QueryDataController {
