d9a99696fdff141e5e931a7ac0e8546f596281f3
cm <chenmeng_1919@163.com>
2022-01-18 10:54:46 +0800

    采用全局异常处理sql查询异常

--- src/main/java/com/a5ibug/lifemap/controller/QueryDataController.java
+++ src/main/java/com/a5ibug/lifemap/controller/QueryDataController.java
@@ -33,18 +33,9 @@ public class QueryDataController {
     @ResponseBody
     public List<Map<String, Object>> test1(@RequestBody FormInLineEntity form) {
         List<Map<String, Object>> list = null;
-        try {
             String sqlstring = form.getSqlstring();
             list = jdbcTemplate.queryForList(sqlstring);
             log.info("sql查询成功：{}",sqlstring);
-        } catch (DataAccessException e) {
-            e.printStackTrace();
-            //获取异常详细信息
-            String message = e.getMessage();
-            //获取异常堆栈
-            String stackTrace = ThrowableUtils.getStackTrace(e);
-            log.info("查询异常，sql执行错误，异常提示为：{}",message);
-        }
         return list;
     }
 }
